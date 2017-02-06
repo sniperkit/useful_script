43261f00dab4ac7abeb4112d6f522ccd10e2b2ed
Jonathan Lee <lamproae@163.com>
2017-02-06 11:47:51 +0000

    Add libevent

--- cmake.sh
+++ cmake.sh
@@ -136,5 +136,7 @@ git clone https://github.com/lamproae/llog.git                                  
 # git clone https://github.com/lamproae/liblw.git                                 lib/liblw
 
 # git clone https://github.com/lamproae/gperftools.git                            lib/gperftools
+# git clone https://github.com/lamproae/libev.git                                 lib/libev
+# git clone https://github.com/lamproae/Libevent.git                              lib/Libevent
 
 
